619d70361bd95cece3472f59c819d1fd7e72c131
Dinh Thanh An <an.dinh@elinext.com>
2017-09-15 18:54:20 +0700

    add sudoku2 solution

--- PracticeAlgorithm/CodeFightsTest.swift
+++ PracticeAlgorithm/CodeFightsTest.swift
@@ -39,4 +39,48 @@ class CodeFightsTest {
         
         return uniqueCharacterString.characters.count > 0 ? uniqueCharacterString.characters.first! : "_"
     }
+    
+    func rotateImage(a: inout [[Int]]) -> [[Int]] {
+        if a.count <= 1 {
+            return a
+        }
+        for n in 0...a.count - 2 {
+            for m in n + 1...a.count - 1 {
+                swap(&a[n][m], &a[m][n])
+            }
+        }
+        for n in 0..<a.count {
+            for m in 0..<a.count / 2 {
+                if m != a.count - 1 - m {
+                    swap(&a[n][m], &a[n][a.count - 1 - m])
+                }
+            }
+        }
+        
+        return a
+    }
+    
+    func sudoku2(grid: [[Character]]) -> Bool {
+        for r in 0..<grid.count{
+            var tempArray = [Int](repeating: 0, count: grid.count + 1)
+            var tempArray2 = [Int](repeating: 0, count: grid.count + 1)
+            for c in 0..<grid.count {
+                if let number = Int("\(grid[r][c])") {
+                    if tempArray[number] == 1 {
+                        return false
+                    }
+                    tempArray[number] = tempArray[number] + 1
+                }
+                
+                if let number = Int("\(grid[c][r])") {
+                    if tempArray2[number] == 1 {
+                        return false
+                    }
+                    tempArray2[number] = tempArray2[number] + 1
+                }
+            }
+        }
+        
+        return true
+    }
 }
