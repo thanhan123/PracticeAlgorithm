6b95a366db17b2615409046db7b09990a471cb79
thanhan123 <thanhan.uit@gmail.com>
2017-09-16 09:49:27 +0700

    finish sudoku2 solution

--- PracticeAlgorithm/CodeFightsTest.swift
+++ PracticeAlgorithm/CodeFightsTest.swift
@@ -32,11 +32,40 @@ class CodeFightsTest {
                 repeatedCharacterString = repeatedCharacterString.appending("\(c)")
             }
         }
-                
+        
         for c in repeatedCharacterString.characters {
             uniqueCharacterString = uniqueCharacterString.replacingOccurrences(of: "\(c)", with: "")
         }
         
         return uniqueCharacterString.characters.count > 0 ? uniqueCharacterString.characters.first! : "_"
+    }
+    
+    func isCryptSolution(crypt: [String], solution: [[Character]]) -> Bool {
+        var solutionDict = [String: String]()
+        
+        for s in solution {
+            solutionDict["\(s[0])"] = "\(s[1])"
+        }
+        
+        var encryptedArray = [Int]()
+        
+        for c in crypt {
+            let encryptString = encrypt(s: c, solution: solutionDict)
+            if encryptString[encryptString.startIndex] == "0" && encryptString.characters.count > 1 {
+                return false
+            }
+            encryptedArray.append(Int("\(encryptString)")!)
+        }
+        
+        return encryptedArray[0] + encryptedArray[1] == encryptedArray[2]
+    }
+    
+    internal func encrypt(s: String, solution: [String: String]) -> String {
+        var result = ""
+        for c in s.characters {
+            result = result.appending(solution["\(c)"]!)
+        }
+        
+        return result
     }
 }
